sentientmonkey/vim-flog

commit 784835d2a2d2ffbe248f3201542ee752eb986d12

fix signs unplacing

plugin/flog.vim
@@ -59,7 +59,7 @@ class Flog
 end
 
 def show_complexity(results = {})
-  VIM.command ":silent sign unplace file=#{VIM::Buffer.current.name}"
+  VIM.command ":silent sign unplace *"
   medium_limit = VIM::evaluate('s:medium_limit')
   high_limit = VIM::evaluate('s:high_limit')
 

commit bed787aee45d56bbce454b9cdd585f102999eafa

Unplace signs only in current buffer
Commit 784835d introduced a bug when you got multiple buffers opened in
multiple windows.

If you unplace signs in all opened windows, they're going to be placed
back by `show_complexity()` only in current window. But all other
windows will jump for a few pixels to the left, which is ugly.

The proper fix for original bug (when even current buffer signs weren't
unplaced) would be to use `*` together with `file`:

```
:sign unplace * file={fname}
		Remove all placed signs in file {fname}.
```

plugin/flog.vim
@@ -63,7 +63,7 @@ class Flog
 end
 
 def show_complexity(results = {})
-  VIM.command ":silent sign unplace *"
+  VIM.command ":silent sign unplace * file=#{VIM::Buffer.current.name}"
   medium_limit = VIM::evaluate('s:medium_limit')
   high_limit = VIM::evaluate('s:high_limit')
 